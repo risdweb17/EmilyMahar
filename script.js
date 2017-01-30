432ea4e698c0b804f45051bc7aaad54d11ca5d11
emahar <emilymahar@Emilys-MacBook-Pro.local>
2017-01-30 12:05:53 -0500

    adjustments

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-$(function() {
-	$("#projects").click(function(){
-    $("#projectsexpanded").toggle();
+$(document).ready(function(){
+	$(".projects").click(function(){
+    	$(".projectsexpanded").fadeToggle();
+	});
+
+	$(".exercises").click(function(){
+    	$(".exercisesexpanded").fadeToggle();
+	});
+
+	$(".responses").click(function(){
+    	$(".responsesexpanded").fadeToggle();
+	});
+
+	$(".codeacademy").click(function(){
+    	$(".codeexpanded").fadeToggle();
+	});
 });
