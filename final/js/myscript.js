8c9ef8c2ab270762c97a4041be052a99238f440a
emahar <emilymahar@emilys-mbp.risd.edu>
2017-02-06 15:20:12 -0500

    changes

--- final/js/myscript.js
+++ final/js/myscript.js
@@ -1,3 +1,16 @@
+$(document).ready(function() {
+	$(".library").click(function(){
+        $(".sublibrary").slideToggle(300);
+    });
+
+    $('.library').click(function() {
+	    $('down').toggle();
+	    $(this).toggleClass("down up");
+	});
+
+});
+
+
 var options = {
   valueNames: [ 'title', 'author' ]
 };
